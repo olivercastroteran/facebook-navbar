beb3d3830eb0e6c53c60791ff35f4fe74a802232
olivercastroteran <olivercastroteran@gmail.com>
2020-08-19 17:04:43 -0400

    dropdown menu added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,31 @@ function App() {
       <NavItem icon={<PlusIcon />} />
       <NavItem icon={<BellIcon />} />
       <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu />
+      </NavItem>
     </Navbar>
+  );
+}
+
+function DropdownMenu() {
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem leftIcon={<CogIcon />} rightIcon={<ChevronIcon />}>
+        Settings
+      </DropdownItem>
+    </div>
   );
 }
 
--- src/NavItem.js
+++ src/NavItem.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NavItem = (props) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+      {open && props.children}
     </li>
   );
 };
