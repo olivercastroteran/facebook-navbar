e2261a58d76c618b7a2d609559fd1a7a409e13dc
olivercastroteran <olivercastroteran@gmail.com>
2020-08-18 23:10:13 -0400

    Implementing svgs as react components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Navbar from './Navbar';
 import NavItem from './NavItem';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 
 function App() {
   return (
     <Navbar>
-      <NavItem icon="🦁" />
-      <NavItem icon="🦁" />
-      <NavItem icon="🦁" />
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
     </Navbar>
   );
 }
